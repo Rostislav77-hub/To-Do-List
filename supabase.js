9d858ec9a464f3d65df9365022d0117076ec0b11
Rostislav77-hub <rostikmatv123@gmail.com>
2026-05-10 18:43:52 +0300

    feat: add Supabase client config

---
+++ supabase.js
@@ -0,0 +1,6 @@
+
+const SUPABASE_URL  = 'https://nahmaxxhaganhbdycooo.supabase.co';  
+const SUPABASE_ANON = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5haG1heHhoYWdhbmhiZHljb29vIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgzNTc5MjIsImV4cCI6MjA5MzkzMzkyMn0.HjB4Gqf7zY3uEcoRmF-yz61XiQT1g39EHuGs1LTrIDo';      
+
+const { createClient } = window.supabase;
+const db = createClient(SUPABASE_URL, SUPABASE_ANON);
